0589c93bdd9e0c55eb8a25c29b9a447570784f44
rdf <rdf@coopeserv.localdomain>
2014-02-20 09:02:08 +0100

    fixed #31

--- capcelera.php
+++ capcelera.php
@@ -1,10 +1,5 @@
 <?php
 
-
-header("Cache-Control: private, must-revalidate, max-age=0");
-header("Pragma: no-cache");
-header("Expires: Fri, 4 Jun 2010 12:00:00 GMT");
-
 if(!($_SESSION)) {
     session_start();
 }
